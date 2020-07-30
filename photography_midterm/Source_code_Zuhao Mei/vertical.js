2fe0df656a4130caeeea78ee838ea116dea495e8
Andrew6324 <60396746+Andrew6324@users.noreply.github.com>
2020-07-30 17:34:43 -0400

    midterm

---
+++ photography_midterm/Source_code_Zuhao Mei/vertical.js
@@ -0,0 +1,27 @@
+var img;
+
+function preload() {
+	img = loadImage("mid_pic01/5.JPG");
+}
+
+function setup() {
+	createCanvas(1920, 1080);
+	background(0);
+	noLoop();
+}
+
+function draw() {
+	background(0);
+	
+	img.loadPixels();
+
+	for (var y = 0; y < img.height; y += 15) {
+		for (var x = 0; x < img.width; x += 15) {
+			var c = img.get(x, y);
+			
+			fill(c);
+			noStroke();
+			rect(x, y, 10, 15);
+		}
+	}
+}
